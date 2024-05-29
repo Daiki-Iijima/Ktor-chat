eee0d46d571d356c7d519e892973582b5813a2c4
Daiki Iijima <daiki.applecreate@gmail.com>
2024-05-29 23:16:28 +0900

    一通りの処理が完成

---
+++ src/main/kotlin/com/djima/Application.kt
@@ -0,0 +1,55 @@
+package com.djima
+
+import com.djima.data.MessageDataSource
+import com.djima.data.MessageDataSourceImpl
+import com.djima.data.model.Message
+import com.djima.plugins.*
+import com.djima.room.RoomController
+import com.djima.routes.chatSocket
+import com.djima.routes.getAllMessage
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import com.mongodb.kotlin.client.coroutine.MongoCollection
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import org.slf4j.LoggerFactory
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    //  データベースの設定
+    val database = getDatabase()
+    val messageCollection: MongoCollection<Message> = database.getCollection("messages")
+    val messageDataSource: MessageDataSource = MessageDataSourceImpl(messageCollection)
+    val roomController = RoomController(messageDataSource)
+
+    configureSockets()
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+    configureRouting(roomController = roomController)
+}
+
+fun getDatabase(): MongoDatabase {
+    val connectionString = System.getenv("MONGODB_CONNECTION_STRING")
+
+    val logger = LoggerFactory.getLogger(Application::class.java)
+    if(connectionString == null) {
+        logger.info("環境変数からMongoDBへのアクセスURLが取得できませんでした")
+    }else{
+        logger.info("MongoDBへのアクセスURLを取得")
+    }
+
+    val client = MongoClient.create(connectionString)
+
+    return client.getDatabase("chatApp")
+}
+
+fun Application.configureRouting(roomController: RoomController) {
+    routing {
+        chatSocket(roomController)
+        getAllMessage(roomController)
+    }
+}
